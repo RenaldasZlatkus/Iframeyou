d80ebe7ee3163182d2589271510ee2b40613d81a
Renaldas Zlatkus <renaldas.zlatkus@gmail.com>
2015-12-02 11:57:30 +0100

    login system

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
 	has_many :settings
 	has_many :websites
+
+    validates :email, uniqueness: true
+	validates :password
+	#validates_presence_of :password, on: :create
 end
