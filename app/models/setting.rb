c827a001671bf15196d644a83911081d6d865f0e
YahNé Ndgo <yndgo@bennington.edu>
2015-12-03 12:48:49 +0100

    added images for the icons in the settings menu fixes #14.

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,11 +1,11 @@
 class Setting < ActiveRecord::Base
 	belongs_to :user
-	after_initialize :defaults
+	# after_initialize :defaults
 
-    def defaults
-		self.frames ||= 1 
-		self.style ||= 1
-		self.skin ||= white
-		self.background ||= white
-    end
+ #    def defaults
+	# 	self.frames ||= 1 
+	# 	self.style ||= 1
+	# 	self.skin ||= white
+	# 	self.background ||= white
+ #    end
 end
