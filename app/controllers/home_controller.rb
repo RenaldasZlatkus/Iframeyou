c6bfe5dc3afe749f69091d2b2d1728d5927c9175
YahNé Ndgo <yndgo@bennington.edu>
2015-12-06 00:55:57 +0100

    style menu form buttons so they appear to be pushed when selected, added home button to style menu, suppressed menu button from showing on home page when current user

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,6 @@ class HomeController < ApplicationController
 
 	def index
 		@user = User.new
-end
-
+	end
 end
 
--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,11 +1,23 @@
 class Setting < ActiveRecord::Base
 	belongs_to :user
-	# after_create :defaults
+	before_update :check_for_http
 
- #    def defaults
-	# 	self.frames ||= 1 
-	# 	self.style ||= 1
-	# 	self.skin ||= "white"
-	# 	self.background ||= "white"
- #    end
+	def check_for_http
+		if self.website1.match(/https?:\/\//) == nil
+			self.website1 = "http://" + self.website1
+		end
+
+		if self.website2.match(/https?:\/\//) == nil
+			self.website2 = "http://" + self.website2
+		end
+
+		if self.website3.match(/https?:\/\//) == nil
+			self.website3 = "http://" + self.website3
+		end
+
+		if self.website4.match(/https?:\/\//) == nil
+			self.website4 = "http://" + self.website4
+		end		
+	end
+
 end
